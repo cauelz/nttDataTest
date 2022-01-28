f879a677ceff1c2c9873d58801a4c802e21389c4
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2022-01-27 21:14:26 -0300

    Trigger em account

--- force-app/main/default/lwc/editDetails/editDetails.js
+++ force-app/main/default/lwc/editDetails/editDetails.js
@@ -4,4 +4,21 @@ export default class EditDetails extends LightningElement {
 
     @api recordId;
 
+
+    errorMethod(event) {
+        let message = event.detail.detail;
+        //do some stuff with message to make it more readable
+        message = "Algo deu errado";
+        this.showToast(TOAST_TITLE_ERROR, message, TOAST_VARIANT_ERROR);
+       this.clearEditMode();
+    }
+
+    showToast(theTitle, theMessage, theVariant) {
+        const event = new ShowToastEvent({
+            title: theTitle,
+            message: theMessage,
+            variant: theVariant
+        });
+     this.dispatchEvent(event);
+    }
 }
